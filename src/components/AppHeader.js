3491856bc32c0bcd9b942fb6eede8d298a953bdc
Robin Tyagi <robin@ict.gnu.ac.in>
2020-04-27 07:19:39 +0000

    hhhhh

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AppHeader() {
 
     return <div>
-        <p className="p-3 text-light h3 bg-primary">Current weather by zip code jjjjjjjjGANPAT HEADER</p>
+        <p className="p-3 text-light h3 bg-primary">Current weather by zip code jjjjjjjj mmmmmmmGANPAT kkHEADER</p>
     </div>
 }
   
